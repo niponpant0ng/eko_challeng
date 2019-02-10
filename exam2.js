7c8684213a929247df71f504d82f30560cbf751b
nipon pantong <nipon.pantong@scb.co.th>
2019-02-10 16:00:06 +0700

    exam2 support 2 case

---
+++ exam2.js
@@ -0,0 +1,15 @@
+const word1 = "foo(bar)"
+console.log(formatWord(word1))
+
+const word2 = "(bar)"
+console.log(formatWord(word2))
+
+function formatWord(realWord) {
+  return realWord.split("(").map(word => {
+    if(word.match(/\)/)) {
+      return [ ...(word.replace(/\)/g, "")) ].reverse().join("")
+    }
+  
+    return word
+  })
+}
